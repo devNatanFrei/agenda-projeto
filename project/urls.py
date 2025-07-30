ecbb829e02e14f18024efa31d13260cbfe36ac58
Natan Freitas <natanael_freitas@outlook.com>
2025-07-30 10:06:14 -0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -21,8 +21,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('', include('contact.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('admin/', admin.site.urls),
+    ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
