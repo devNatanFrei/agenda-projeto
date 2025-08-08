3717248a187da3851ee489e06447bd5316f93d0e
Natan Freitas <natanael_freitas@outlook.com>
2025-08-08 20:34:57 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -83,6 +83,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
